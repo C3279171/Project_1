0e0195039234bd40bbfcc73ab30f0d1ad0989f2d
C3279171 <48077964+C3279171@users.noreply.github.com>
2019-04-23 10:34:16 +1000

    Update main.c

--- main.c
+++ main.c
@@ -72,3 +72,68 @@ printf("Encrypted message: %s", strupr(message));
 }
 	return 0;
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char shift_tbl_u[27] = "GHIJKLMNOPQRSTUVWXYZABCDEF"; 
+char shift_tbl_l[27] = "ghijklmnopqrstuvwxyzabcdef"; 
+char encrypt(char inchar)
+{
+        if(isalpha(inchar) && isupper(inchar))
+    {
+        return shift_tbl_u[inchar-'A'];
+    }
+    else if(isalpha(inchar) && islower(inchar)) 
+    {
+        return shift_tbl_l[inchar-'a'];
+    }
+    return inchar; // non alphabetic = no change;
+}
+
+
+int main (void)
+{
+    FILE *fp = fopen("code.txt", "rb+");
+    char *buffer = NULL;    
+
+
+    size_t s_file;
+    int i;
+
+    if(!fp)
+    {
+        puts("Could not open code.txt");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(fp, 0, SEEK_END);
+    s_file = ftell(fp);
+    rewind(fp);
+    buffer = malloc(s_file + 1);
+    if(!buffer)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    fread(buffer, 1, s_file, fp);
+
+    fseek(fp, 0, SEEK_SET);
+
+    for(i = 0; i < s_file; i++)
+    {
+        char c = encrypt(buffer[i]);
+        fwrite(&c, 1, 1, fp);
+    }
+    free(buffer);
+    buffer = NULL;
+
+    fclose(fp);
+
+    return 0;
+}
+*\
